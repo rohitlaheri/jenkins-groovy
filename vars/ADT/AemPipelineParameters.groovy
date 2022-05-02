10b3f78ee7f79aa21ab68e55e7bb8801643ad359
Rohit Laheri <rohit.laheri@us.brainscale.com>
2022-05-03 03:58:31 +0530

    class

---
+++ vars/ADT/AemPipelineParameters.groovy
@@ -0,0 +1,38 @@
+#!/usr/bin/groovy
+import pipelinejob.pipelineConstants
+
+public class AemPipelineParameters {
+    String imageTag
+    String previousTarget
+    AemPipelineParameters(String pReleaseBranch, String previous_Target, String image_tag) {
+        releaseBranch = pReleaseBranch
+        this.imageTag = image_tag
+        this.previousTarget = previous_Target
+    }
+
+    final String releaseBranch
+
+   /* def String[] getAllDeploymentPackageZipFiles() {
+        return ['Zip1, Zip2'] as String[]
+    }
+*/
+    def String getDeploymentTarget() {
+        println("inside getDeploymentTarget")
+        if (previousTarget == '') {
+            return pipelineConstants.DevEnv
+        }
+        else if (previousTarget == 'DIT')
+            return pipelineConstants.SIT1
+        else
+            return pipelineConstants.PRD1
+    }
+    def String getArtifactoryUrl()
+    {
+         if(imageTag.equalsIgnoreCase('latest'))
+         {
+             return "create-artifactory-url"
+         }
+         else
+             return imageTag
+    }
+}
