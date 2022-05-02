b58bf71eca60ddc8597ce9e10c35c5fda14060dc
Rohit Laheri <rohit.laheri@us.brainscale.com>
2022-05-03 04:11:25 +0530

    class

--- src/pipelinejob/Factories/AEMBuildFactory.groovy
+++ src/pipelinejob/Factories/AEMBuildFactory.groovy
@@ -7,31 +7,6 @@ public interface BuildAemCode {
     void runBuild();
 }
 
-//abstract factory class
-public abstract class AEMBuildAbstractFactory {
-    abstract BuildAemCode getBuild(String environment) ;
-}
-
-//Factory class extending abstract factory  to generate object of concrete class
-public class AEMBuildFactory extends AEMBuildAbstractFactory {
-    @Override
-    public BuildAemCode getBuild(String environment) {
-        if (environment.equalsIgnoreCase("dev")) {
-            return new NonProdBuild();
-        } else if (environment.equalsIgnoreCase("prod")) {
-            return new ProductionBuild();
-        }
-        return null;
-    }
-}
-
-//Create a Factory generator class to get factories
-public class FactoryProducer {
-    public static AEMBuildAbstractFactory getFactory(){
-        return new AEMBuildFactory();
-    }
-}
-
 //concrete class implementing interface BuildAemCode
 public class NonProdBuild implements BuildAemCode {
     /*private AemPipelineParameters _parameters
@@ -72,6 +47,33 @@ public class ProductionBuild implements BuildAemCode {
         System.out.println("Inside ProductionBuild::runBuild() method.");
     }
 }
+
+//abstract factory class
+public abstract class AEMBuildAbstractFactory {
+    abstract BuildAemCode getBuild(String environment) ;
+}
+
+//Factory class extending abstract factory  to generate object of concrete class
+public class AEMBuildFactory extends AEMBuildAbstractFactory {
+    @Override
+    public BuildAemCode getBuild(String environment) {
+        if (environment.equalsIgnoreCase("dev")) {
+            return new NonProdBuild();
+        } else if (environment.equalsIgnoreCase("prod")) {
+            return new ProductionBuild();
+        }
+        return null;
+    }
+}
+
+//Create a Factory generator class to get factories
+public class FactoryProducer {
+    public static AEMBuildAbstractFactory getFactory(){
+        return new AEMBuildFactory();
+    }
+}
+
+
 
 
 
--- vars/Constants.groovy
+++
@@ -1,7 +0,0 @@
-
-public final class pipelineConstants {
-    public static final String DevEnv = 'DIT'
-    public static final String SIT1 = 'SIT-FIRST'
-    public static final String PRD1 = 'PRODUCTION-EAST'
-
-}
