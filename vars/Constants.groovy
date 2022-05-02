02bbfda983da5789d9b9dd019953122062be2436
Rohit Laheri <rohit.laheri@us.brainscale.com>
2022-05-03 04:01:25 +0530

    class

---
+++ vars/Constants.groovy
@@ -0,0 +1,7 @@
+
+public final class pipelineConstants {
+    public static final String DevEnv = 'DIT'
+    public static final String SIT1 = 'SIT-FIRST'
+    public static final String PRD1 = 'PRODUCTION-EAST'
+
+}
