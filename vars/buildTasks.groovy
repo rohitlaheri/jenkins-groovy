71f7224d1e38df41596daebbac60101a9ed4d423
Rohit Laheri <rohit.laheri@us.brainscale.com>
2022-05-04 03:25:57 +0530

    shared lib

--- vars/buildTasks.groovy
+++ vars/buildTasks.groovy
@@ -1,35 +1,11 @@
 #!/usr/bin/groovy
-
-
+import  pipelinejob.Factories.AEMBuildAbstractFactory
+import pipelinejob.Factories.*
 def call()
 {
     echo "inside build"
-    pipeline {
-        agent any
-        stages {
-            stage("build from lib") {
-                steps {
-                    script {
-                        echo "testing from lib"
-                    }
-                }
-
-            }
-        }
-    }
-    /*AEMBuildAbstractFactory aemBuildFactory = FactoryProducer.getFactory()
+    AEMBuildAbstractFactory aemBuildFactory = FactoryProducer.getFactory()
     BuildAemCode buildAem = aemBuildFactory.getBuild('dev')
-    buildAem.runBuild()*/
+    buildAem.runBuild()
 }
-return this
 
-
-/*def callParam()
-{
-    def param = new AemPipelineParameters('release2.2.0', 'DIT', 'latest' )
-    println("inside the call param")
-
-    def aurl = param.releaseBranch
-    println(aurl)
-}*/
-//return this
