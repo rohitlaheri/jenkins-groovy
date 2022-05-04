9ef42d0c94d599cf258be16a4d8ce5a87fdcd770
arpitha-kepu <57133400+arpitha-kepu@users.noreply.github.com>
2022-05-04 16:49:44 +0530

    Logs

--- src/pipelinejob/Factories/FactoryProducer.groovy
+++ src/pipelinejob/Factories/FactoryProducer.groovy
@@ -3,6 +3,8 @@ package pipelinejob.Factories
 //Create a Factory generator class to get factories
 public class FactoryProducer {
     public static AEMBuildAbstractFactory getFactory(){
+        echo "Returning Factory"
         return new AEMBuildFactory();
+        echo "Factory Returned"
     }
 }
