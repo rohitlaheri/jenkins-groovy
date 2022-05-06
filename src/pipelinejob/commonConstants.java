15bf671eb586cc47aedbe54e25fe08bf3327692d
Shreyas Gandhe <g.shreyas@outlook.com>
2022-05-06 18:43:21 +0530

    Added common constant

--- src/pipelinejob/commonConstants.java
+++ src/pipelinejob/commonConstants.java
@@ -1,5 +1,5 @@
 package pipelinejob;
 
 public class commonConstants {
-        static final String MYCONSTANT = "CommonConstant";
+        public static final String MYCONSTANT = "CommonConstant";
 }
