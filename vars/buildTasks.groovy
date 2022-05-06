834eec05fe5206799df18bedd923fb9c71be4b41
Shreyas Gandhe <g.shreyas@outlook.com>
2022-05-06 17:45:49 +0530

    Logger added for testing

--- vars/buildTasks.groovy
+++ vars/buildTasks.groovy
@@ -2,9 +2,18 @@
 
 import pipelinejob.Factories.BuildFactory.Configuration.*
 
+import java.util.logging.ConsoleHandler
+import java.util.logging.Level
+import java.util.logging.Logger
 
 def call()
 {
+    def logger = Logger.getLogger("")
+    logger.setLevel(Level.FINEST)
+    logger.addHandler (new ConsoleHandler())
+
+    logger.info("This printed")
+
     echo "inside build"
     BuildAbstractFactory aemBuildFactory = BuildFactoryProducer.getFactory(this)
     echo "Producer called"
