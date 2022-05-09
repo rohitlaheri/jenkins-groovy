56a99ea1f34d2d259621b5843ea03513229e6aa8
Rohit Laheri <rohit.laheri@us.brainscale.com>
2022-05-09 13:35:07 +0530

    api-param-test

--- vars/mediator.groovy
+++ vars/mediator.groovy
@@ -1,36 +1,39 @@
 #!/usr/bin/groovy
 
-def call() {
+def call(param) {
+    def tempParam = param
     pipeline {
         agent any
         stages {
             stage('scm checkout'){
                 steps{
-                    checkout([$class: 'GitSCM', branches: [[name: '*/dev']], extensions: [], userRemoteConfigs: [[url: '']]])
+                    //checkout([$class: 'GitSCM', branches: [[name: '*/dev']], extensions: [], userRemoteConfigs: [[url: '']]])
                 }
             }
 
             stage('build') {
                 steps {
                     script {
-                        buildTasks.call()
+                        echo tempParam
+                        //buildTasks.call()
                     }
                 }
             }
-            stage('scan') {
+            /*stage('scan') {
                 steps {
                     script {
                         scanTasks.call()
                     }
                 }
-            }
-            stage('release') {
+            }*/
+            /*stage('release') {
+
                 steps {
                     script {
                         deployTasks.call()
                     }
                 }
-            }
+            }*/
         }
     }
 }
