cfb7a1c0294b97f3669be23293150d594b9bc1c9
Shreyas Gandhe <g.shreyas@outlook.com>
2022-05-10 18:02:56 +0530

    Added new logging mechanism

--- vars/buildTasks.groovy
+++ vars/buildTasks.groovy
@@ -5,14 +5,12 @@ import io.vz.pipelinejob.Factories.BuildFactory.Configuration.*
 def call()
 {
     echo "inside build"
-    logger.debug("Logger printed this");
     BuildAbstractFactory aemBuildFactory = BuildFactoryProducer.getFactory(this, logger)
     echo "Producer called"
     BuildCode buildAem = aemBuildFactory.getBuilder('AEM')
     echo "Factory Generated"
     buildAem.runBuild()
     echo "Completed"
-    logger.debug("Logger Completed");
 }
 //return this
 
