beff2f7a9cd1a26b0a86170ffc6a0403814ba44f
Rohit Laheri <rohit.laheri@us.brainscale.com>
2022-05-10 19:47:28 +0530

    shell script

--- src/io/vz/pipelinejob/Factories/BuildFactory/AEMBuilder.groovy
+++ src/io/vz/pipelinejob/Factories/BuildFactory/AEMBuilder.groovy
@@ -19,10 +19,10 @@ public class AEMBuilder implements BuildCode {
     //region Methods for build steps --start
     def execShell()
     {
-        def script = "io/vz/pipelinejob/Factories/BuildFactory/testscript.sh"
+       /* def script = "io/vz/pipelinejob/Factories/BuildFactory/testscript.sh"
         def std_out = new StringBuilder()
-        def std_err = new StringBuilder()
-        steps.echo "ls"
+        def std_err = new StringBuilder()*/
+        //steps.echo ls
         //def proc = script.execute()
         //proc.consumeProcessOutput(std_out, std_err)
 
@@ -36,7 +36,9 @@ public class AEMBuilder implements BuildCode {
      public void runBuild() {
 
         steps.echo "echo from class"
-        execShell();
+        steps.echo ls
+        steps.sh "ls"
+        //execShell();
 
     }
 }
