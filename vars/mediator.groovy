12fc9a889331c075b3a999086a491c7ffe3d7ec2
Shreyas Gandhe <g.shreyas@outlook.com>
2022-05-10 15:29:27 +0530

    Added new logging mechanism

--- vars/mediator.groovy
+++ vars/mediator.groovy
@@ -1,14 +1,17 @@
 #!/usr/bin/groovy
-
+import io.vz.pipelinejob.Utils.Logger
 def call(param) {
     def tempParam = param
     def buildStage= false
+    def logger = new Logger(this, true)
+
     pipeline {
         agent any
         stages {
             stage('scm checkout'){
                 steps{
                     //checkout([$class: 'GitSCM', branches: [[name: '*/dev']], extensions: [], userRemoteConfigs: [[url: '']]])
+                    logger.debug("log checkout")
                     echo "git checkout"
                 }
             }
