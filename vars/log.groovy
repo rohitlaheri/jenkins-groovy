8d717cf6e0f7cd9c009a01eafcdb936e2859640a
Shreyas Gandhe <g.shreyas@outlook.com>
2022-05-12 17:56:06 +0530

    Improved logs

--- vars/log.groovy
+++ vars/log.groovy
@@ -1,14 +1,16 @@
 import java.util.*;
-def now = new Date()
 
 def info(message) {
+    def now = new Date()
     echo "INFO: ${now} -- ${message}"
 }
 
 def warning(message) {
+    def now = new Date()
     echo "WARNING: ${now} -- ${message}"
 }
 
 def error(message) {
+    def now = new Date()
     echo "WARNING: ${now} -- ${message}"
 }
