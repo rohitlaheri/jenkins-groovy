9a0ef523f22b74da08faac9afdf090426f404865
Rohit Laheri <rohit.laheri@us.brainscale.com>
2022-05-17 10:18:07 +0530

    testing build

--- src/io/vz/pipelinejob/Factories/BuildFactory/AEMBuilder.groovy
+++ src/io/vz/pipelinejob/Factories/BuildFactory/AEMBuilder.groovy
@@ -47,8 +47,9 @@ public class AEMBuilder implements BuildCode {
         steps.echo "echo from class"
         boolean SRI_Flag = param.SRI
         String module = "ONEVZ-SOE-AEM-DIGITAL-NEXT"
-        def shell = libraryResource 'configuration.json'
-        steps.echo shell
+        def shell = libraryResource('configuration.json')
+        steps.echo "${shell}"
+        steps.echo "$shell"
         //mavenBuild(SRI_Flag, module)
     }
 }
