6689d5e53209a674b323b442cbd18f5c4ef7f6aa
arpitha-kepu <57133400+arpitha-kepu@users.noreply.github.com>
2022-05-19 14:34:02 -0400

    Update ScanCodeFactoryProducer.groovy

--- src/io/vz/pipelinejob/Factories/ScanFactory/Configuration/ScanCodeFactoryProducer.groovy
+++ src/io/vz/pipelinejob/Factories/ScanFactory/Configuration/ScanCodeFactoryProducer.groovy
@@ -2,7 +2,7 @@ package io.vz.pipelinejob.Factories.ScanFactory.Configuration
 
 //Create a Factory generator class to get factories
 public class ScanCodeFactoryProducer {
-    public static ScanCodeAbstractFactory getFactory(steps){
+    public ScanCodeAbstractFactory getFactory(steps){
         print "Returning Factory......"
         return new ScanCodeFactory(steps)
 
