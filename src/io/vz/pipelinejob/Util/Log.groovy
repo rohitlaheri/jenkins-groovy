c3c35d6f00de813e4a8b2b36ca0defde4c911ea0
Shreyas Gandhe <g.shreyas@outlook.com>
2022-05-19 19:46:18 +0530

    Improved logs

--- src/io/vz/pipelinejob/Util/Log.groovy
+++ src/io/vz/pipelinejob/Util/Log.groovy
@@ -32,7 +32,7 @@ class Log implements Serializable {
     private def printDetails(level, message) {
         def now = new Date()
         //println(level + ": " + now + ": " + message)
-        def output = "[${level.label}] [${now}]: " << String.valueOf(body)
+        def output = "[${level}] [${now}]: " << String.valueOf(message)
         steps.echo output.toString()
     }
 
