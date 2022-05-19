45aab180cf10597b0c5ae63cdca96cb288f234f8
Rohit Laheri <rohit.laheri@us.brainscale.com>
2022-05-19 21:24:40 +0530

    mediator

--- src/io/vz/pipelinejob/Factories/CheckOutFactory/GitLabCheckOut.groovy
+++ src/io/vz/pipelinejob/Factories/CheckOutFactory/GitLabCheckOut.groovy
@@ -7,6 +7,24 @@ public class GitLabCheckOut implements CheckOutCode {
 
     def GitLabCheckOut(steps) { this.steps = steps }
 
+
+    def checkoutGitRepository(repoURL, repoBranch, credentialsId = 'gitlab', poll = true, timeout = 10, depth = 0, reference = ''){
+        //def branch_name = reference ? 'FETCH_HEAD' : "*/${branch}"
+            checkout(
+                    changelog:true,
+                    poll: poll,
+                    scm: [
+                            $class: 'GitSCM',
+                            branches: [[name: repoBranch]],
+                            doGenerateSubmoduleConfigurations: false,
+                            extensions: [
+                                    [$class: 'CheckoutOption', timeout: timeout],
+                                    [$class: 'CloneOption', depth: depth, noTags: false, shallow: depth > 0, timeout: timeout]],
+                            submoduleCfg: [],
+                            userRemoteConfigs: [[url: repoURL, credentialsId: credentialsId, refspec: reference]]]
+            )
+        
+    }
     @Override
     public void checkOut(String repoURL, String repoBranch) {
         print("Inside Gitlab::Checkout() method.")
@@ -16,7 +34,8 @@ public class GitLabCheckOut implements CheckOutCode {
         //steps.checkout([$class: 'GitSCM', branches: [[name: $param.repoBranch]], extensions: [], userRemoteConfigs: [[credentialsId: 'gitlab', url: param.repoUrl]]])
         steps.echo repoBranch
         steps.echo repoURL
-        steps.checkout([$class: 'GitSCM', branches: [[name: repoBranch]], extensions: [], userRemoteConfigs: [[credentialsId: 'gitlab', url: repoURL]]])
+        checkoutGitRepository(repoURL, repoBranch, 'gitlab', true, 10, 0, '')
+        //steps.checkout([$class: 'GitSCM', branches: [[name: repoBranch]], extensions: [], userRemoteConfigs: [[credentialsId: 'gitlab', url: repoURL]]])
 
     }
 }
