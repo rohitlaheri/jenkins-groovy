e4c8d4bd68981db9770cf3aad15bcff3ea7a1762
Rohit Laheri <rohit.laheri@us.brainscale.com>
2022-05-19 09:43:18 +0530

    mediator

--- src/io/vz/pipelinejob/Factories/CheckOutFactory/Configuration/CheckOutFactoryProducer.groovy
+++ src/io/vz/pipelinejob/Factories/CheckOutFactory/Configuration/CheckOutFactoryProducer.groovy
@@ -1,7 +1,7 @@
 package io.vz.pipelinejob.Factories.CheckOutFactory.Configuration
 
 //Create a Factory generator class to get factories
-public class CheckOutFactoryFactoryProducer {
+public class CheckOutFactoryProducer {
     public static CheckOutAbstractFactory getFactory(steps){
         print "Returning Factory......"
         return new CheckOutFactory(steps)
