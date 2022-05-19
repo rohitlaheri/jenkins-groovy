3bb03e7ca407b3f618332fae83fc64d9b6dc6198
Shreyas Gandhe <g.shreyas@outlook.com>
2022-05-19 19:00:46 +0530

    Improved logs

--- vars/log.groovy
+++ vars/log.groovy
@@ -1,6 +1,8 @@
 import java.util.*;
 import io.vz.pipelinejob.Util.Log
+import io.vz.pipelinejob.Util.LogLevel
 
+/*
 def info(message) {
     Log log = new Log(steps)
     log.info(message)
@@ -14,4 +16,37 @@ def warning(message) {
 def error(message) {
     Log log = new Log(steps)
     log.error(message)
+}*/
+
+def error(msg, dump = null) {
+    message ERROR, msg, dump
+}
+
+def warn(msg, dump = null) {
+    message WARNING, msg, dump
+}
+
+def info(msg, dump = null) {
+    message INFO, msg, dump
+}
+
+def debug(msg, dump = null) {
+    message DEBUG, msg, dump
+}
+
+def message(LogLevel level, Object body, Object dump) {
+    if (BuildData.instance.logLevel.encompasses(level)) {
+        def message = "[${level.label}] " << String.valueOf(body)
+        if (dump) {
+            message << "\n"
+            if (dump instanceof Throwable) {
+                StringWriter w = new StringWriter()
+                StringUtils.printStackTrace(dump, new PrintWriter(w))
+                message.append(w.buffer)
+            } /*else {
+                message << new PrettyPrinter(dump).incrementIndent().toPrettyPrint()
+            }*/
+        }
+        echo message.toString()
+    }
 }
