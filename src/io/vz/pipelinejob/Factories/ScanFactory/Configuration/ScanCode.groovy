a1bf2688535489568cd9de958571f15fdfb0cac8
arpitha-kepu <57133400+arpitha-kepu@users.noreply.github.com>
2022-05-20 12:15:11 -0400

    Update ScanCode.groovy

--- src/io/vz/pipelinejob/Factories/ScanFactory/Configuration/ScanCode.groovy
+++ src/io/vz/pipelinejob/Factories/ScanFactory/Configuration/ScanCode.groovy
@@ -2,5 +2,5 @@ package io.vz.pipelinejob.Factories.ScanFactory.Configuration
 
 //interface ScanCode
 public interface ScanCode {
-    void runScan();
+    void runScan(String requestID,String repoBranch);
 }
