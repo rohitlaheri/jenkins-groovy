a8e25f07a8abbea1b4a49b6e6e6c74e478cafd49
Rohit Laheri <rohit.laheri@us.brainscale.com>
2022-05-23 04:00:27 +0530

    sonar-pr

--- vars/buildTasks.groovy
+++ vars/buildTasks.groovy
@@ -7,7 +7,7 @@ def call(def param)
 {
     def appType = param.appType
     //def shell = libraryResource 'configuration.json'
-    writeFile file:'configuration.json', text:libraryResource("configuration.json")
+
     env.TestName="Rohit"
     echo " test env name ${TestName}"
     echo appType
