810cd448399dd5deb8f3603848d689bd2a4e5207
Rohit Laheri <rohit.laheri@us.brainscale.com>
2022-05-23 09:45:41 +0530

    config json

--- src/io/vz/pipelinejob/Helpers/CommonHelper.groovy
+++ src/io/vz/pipelinejob/Helpers/CommonHelper.groovy
@@ -8,13 +8,18 @@ import org.json.simple.parser.*;
 public class CommonHelper implements  Serializable{
     def jsonParser
     def steps
+    def jObject
+
     CommonHelper(steps){
         this.jsonParser= new JsonSlurper()
         this.steps = steps
+        jObject = this.steps.readJSON file: 'dir/input.json'
+        steps.echo jObject
     }
 
     public def getModuleConfig(){
         steps.echo "inside get module"
+        steps.echo jObject
         def deserializedJson= this.jsonParser.parse(new File('configuration.json'))
         return deserializedJson.module.AEMModule;
     }
@@ -26,13 +31,6 @@ public class CommonHelper implements  Serializable{
         def deserializedJson= this.jsonParser.parse(new File('configuration.json'))
         return deserializedJson.AEMServer.QAServer;
     }
-    public jsonParser() {
-        // parsing file "JSONExample.json"
-        Object ob = new JSONParser().parse(new FileReader("configuration.json"));
-        // typecasting ob to JSONObject
-        JSONObject js = (JSONObject) ob;
-        steps.echo js
 
-    }
 }
 
