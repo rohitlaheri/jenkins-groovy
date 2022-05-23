7d78641a1f4270e2c77d03abcf222467b81547b8
Arpitha KS <arpitha.ks@us.brainscale.com>
2022-05-23 14:32:21 -0500

    name space issues

--- src/io/vz/pipelinejob/Factories/BuildFactory/Configuration/BuildFactory.groovy
+++ src/io/vz/pipelinejob/Factories/BuildFactory/Configuration/BuildFactory.groovy
@@ -2,6 +2,7 @@
 package io.vz.pipelinejob.Factories.BuildFactory.Configuration
 
 import io.vz.pipelinejob.Factories.BuildFactory.AEMBuilder
+import io.vz.pipelinejob.Model.*
 
 //Factory class extending abstract factory to generate object of concrete class
 public class BuildFactory extends BuildAbstractFactory {
