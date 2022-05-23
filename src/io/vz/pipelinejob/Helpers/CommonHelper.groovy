f6a033ab99a38c864ddc828c05c4b18ee7dddaf1
Arpitha KS <arpitha.ks@us.brainscale.com>
2022-05-23 14:28:43 -0500

    Updated Json reader

--- src/io/vz/pipelinejob/Helpers/CommonHelper.groovy
+++ src/io/vz/pipelinejob/Helpers/CommonHelper.groovy
@@ -11,18 +11,20 @@ public class CommonHelper implements  Serializable{
 //    def jObject
 
     CommonHelper(steps){
-        //this.jsonParser= new JsonSlurper()
         this.steps = steps
-
     }
 
     public def getModuleConfig(){
         this.steps.writeFile file:'configuration.json', text:this.steps.libraryResource("configuration.json")
         this.steps.sh "ls"
-        def jObject =  jsonParse(this.steps.readFile("configuration.json"))
-        steps.echo "inside get module"
-        steps.echo jObject
-        steps.sh "cat configuration.json"
+        def json = this.steps.readFile("configuration.json")
+        steps.echo "Json Value :"+json
+        
+        // def jObject =  jsonParse(this.steps.readFile("configuration.json"))
+        // steps.echo "inside get module"
+        // steps.echo jObject
+        // steps.sh "cat configuration.json"
+
         //def deserializedJson= this.jsonParser.parse(new File('configuration.json'))
         //return deserializedJson.module.AEMModule;
     }
@@ -34,6 +36,7 @@ public class CommonHelper implements  Serializable{
         def deserializedJson= this.jsonParser.parse(new File('configuration.json'))
         return deserializedJson.AEMServer.QAServer;
     }
+
     @NonCPS
     def jsonParse(def json) {
         new groovy.json.JsonSlurperClassic().parseText(json)
