64fc05797794f57e8771301bf64d6293e8e36da4
Rohit Laheri <rohit.laheri@us.brainscale.com>
2022-05-24 09:52:00 +0530

    sonar analysis env variable - test

--- src/io/vz/pipelinejob/Factories/ScanFactory/Configuration/ScanCodeFactory.groovy
+++ src/io/vz/pipelinejob/Factories/ScanFactory/Configuration/ScanCodeFactory.groovy
@@ -2,6 +2,7 @@ package io.vz.pipelinejob.Factories.ScanFactory.Configuration
 
 import io.vz.pipelinejob.Factories.ScanFactory.BlackDuckScan
 import io.vz.pipelinejob.Factories.ScanFactory.SonarQubeScan
+import io.vz.pipelinejob.Model.*
 
 public class ScanCodeFactory extends ScanCodeAbstractFactory {
     private def steps
