eb1927facb0f8d9c76d2fd6c42b666673954e8c2
Rohit Laheri <rohitdammy@gmail.com>
2022-06-07 06:14:07 +0530

    Update mediator.groovy

--- vars/mediator.groovy
+++ vars/mediator.groovy
@@ -32,78 +32,78 @@ def call(body) {
                     }
                 }
             }
-            /*
-            stage('build') {
-                steps {
-                    updateGitlabCommitStatus name: 'build', state: 'pending'
-                    script {
-                            buildTasks.call(pipelineParams)
-                            //updateGitlabCommitStatus name: 'build', state: 'success'
+            
+            // stage('build') {
+            //     steps {
+            //         updateGitlabCommitStatus name: 'build', state: 'pending'
+            //         script {
+            //                 buildTasks.call(pipelineParams)
+            //                 //updateGitlabCommitStatus name: 'build', state: 'success'
 
 
-                    }
-                }
-            }
-            stage('scan') {
-                steps {
-                    updateGitlabCommitStatus name: 'scan', state: 'pending'
-                    script {
-                         //try{
-                            scanTasks.call(pipelineParams)
-                            updateGitlabCommitStatus name: 'scan', state: 'success'
-                           //}
-                        /*catch (e) {*/
-                            //echo $e
-                            //updateGitlabCommitStatus name: 'scan', state: 'failed'
-                            //currentBuild.result = 'FAILURE'
-                        //}
-                    }
-                }
-            //}
-            /*stage('upload to Artifactory') {
-                steps {
-                    script {
-                        artiFactory.call(pipelineParams.SRI)
-                        //updateGitlabCommitStatus name: 'pipeline Succedded', state: 'success'
-                    }
-                }
-            }*/
-            /*
-            stage("Quality gate") {
-                steps {
-                    updateGitlabCommitStatus name: 'Quality gate', state: 'pending'
-                    script {
-                        try{
-                            //waitForQualityGate abortPipeline: true
-                            timeout(time: 2, unit: 'MINUTES') {
-                                def qualityGate = waitForQualityGate()
-                                if (qualityGate.status == 'ERROR') {
-                                    currentBuild.result = 'FAILURE'
-                                    updateGitlabCommitStatus name: 'Quality gate', state: 'failed'
-                                }
-                                else {
-                                    updateGitlabCommitStatus name: 'Quality gate', state: 'success'
-                                    updateGitlabCommitStatus name: 'build', state: 'success'
-                                    currentBuild.result = 'SUCCESS'
-                                }
-                            }
+            //         }
+            //     }
+            // }
+            // stage('scan') {
+            //     steps {
+            //         updateGitlabCommitStatus name: 'scan', state: 'pending'
+            //         script {
+            //              //try{
+            //                 scanTasks.call(pipelineParams)
+            //                 updateGitlabCommitStatus name: 'scan', state: 'success'
+            //                //}
+            //             /*catch (e) {*/
+            //                 //echo $e
+            //                 //updateGitlabCommitStatus name: 'scan', state: 'failed'
+            //                 //currentBuild.result = 'FAILURE'
+            //             //}
+            //         }
+            //     }
+            // }
+            // stage('upload to Artifactory') {
+            //     steps {
+            //         script {
+            //             artiFactory.call(pipelineParams.SRI)
+            //             //updateGitlabCommitStatus name: 'pipeline Succedded', state: 'success'
+            //         }
+            //     }
+            // }
+            
+            // stage("Quality gate") {
+            //     steps {
+            //         updateGitlabCommitStatus name: 'Quality gate', state: 'pending'
+            //         script {
+            //             try{
+            //                 //waitForQualityGate abortPipeline: true
+            //                 timeout(time: 2, unit: 'MINUTES') {
+            //                     def qualityGate = waitForQualityGate()
+            //                     if (qualityGate.status == 'ERROR') {
+            //                         currentBuild.result = 'FAILURE'
+            //                         updateGitlabCommitStatus name: 'Quality gate', state: 'failed'
+            //                     }
+            //                     else {
+            //                         updateGitlabCommitStatus name: 'Quality gate', state: 'success'
+            //                         updateGitlabCommitStatus name: 'build', state: 'success'
+            //                         currentBuild.result = 'SUCCESS'
+            //                     }
+            //                 }
 
-                        }catch (Exception e) {
-                            updateGitlabCommitStatus name: 'Quality gate', state: 'failed'
-                            updateGitlabCommitStatus name: 'build', state: 'success'
-                        }
-                    }
+            //             }catch (Exception e) {
+            //                 updateGitlabCommitStatus name: 'Quality gate', state: 'failed'
+            //                 updateGitlabCommitStatus name: 'build', state: 'success'
+            //             }
+            //         }
 
-                }
-            }
-            */
-            /*stage('release') {
-                steps {
-                    script {
-                        deployTasks.call()
-                    }
-                }
-            }*/
+            //     }
+            // }
+            
+            // stage('release') {
+            //     steps {
+            //         script {
+            //             deployTasks.call()
+            //         }
+            //     }
+            // }
         }
     }
 }
