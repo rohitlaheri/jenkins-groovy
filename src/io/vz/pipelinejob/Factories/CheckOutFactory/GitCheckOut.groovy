633835f5eafd0b3d6028b4c5d6b1e1cbde854405
Rohit Laheri <rohitdammy@gmail.com>
2022-06-17 08:19:22 +0530

    Update GitCheckOut.groovy

--- src/io/vz/pipelinejob/Factories/CheckOutFactory/GitCheckOut.groovy
+++ src/io/vz/pipelinejob/Factories/CheckOutFactory/GitCheckOut.groovy
@@ -54,7 +54,7 @@ public class GitCheckOut implements CheckOutCode, Serializable  {
         this.steps.echo param.branch
         this.steps.echo param.repoURL
         checkoutGit(param)
-        def rep = this.steps.changeset pattern: "**/pom.xml"
-        this.steps.echo rep
+        def changeLogSets = currentBuild.changeSets
+        this.steps.echo changeLogSets
     }
 }
