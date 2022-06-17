c23c762f6e66d1a68d67fbfdfe03b1c3d44bca56
Rohit Laheri <rohitdammy@gmail.com>
2022-06-17 08:23:47 +0530

    Update GitCheckOut.groovy

--- src/io/vz/pipelinejob/Factories/CheckOutFactory/GitCheckOut.groovy
+++ src/io/vz/pipelinejob/Factories/CheckOutFactory/GitCheckOut.groovy
@@ -55,6 +55,18 @@ public class GitCheckOut implements CheckOutCode, Serializable  {
         this.steps.echo param.repoURL
         checkoutGit(param)
         def changeLogSets = this.steps.currentBuild.changeSets
-        this.steps.echo changeLogSets
+        for (int i = 0; i < changeLogSets.size(); i++) {
+           def entries = changeLogSets[i].items
+           for (int j = 0; j < entries.length; j++) {
+               def entry = entries[j]
+               echo "${entry.commitId} by ${entry.author} on ${new Date(entry.timestamp)}: ${entry.msg}"
+               def files = new ArrayList(entry.affectedFiles)
+               for (int k = 0; k < files.size(); k++) {
+                   def file = files[k]
+                   steps.echo " ${file.editType.name} ${file.path}"
+               }
+            }
+           }
+        this.steps.echo changeLogSets.toString()
     }
 }
