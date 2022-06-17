9344882f75f4338662b739379c001c62a632062a
Rohit Laheri <rohitdammy@gmail.com>
2022-06-17 08:47:45 +0530

    Update GitCheckOut.groovy

--- src/io/vz/pipelinejob/Factories/CheckOutFactory/GitCheckOut.groovy
+++ src/io/vz/pipelinejob/Factories/CheckOutFactory/GitCheckOut.groovy
@@ -63,7 +63,7 @@ for (int i = 0; i < changeLogSets.size(); i++) {
         def files = new ArrayList(entry.affectedFiles)
         for (int k = 0; k < files.size(); k++) {
             def file = files[k]
-            steps.echo "changes:   ${file.editType.name}"
+            steps.echo "changes:   ${file.path}"
         }
     }
 }
