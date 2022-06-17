4f3bfd13fab00db4dec105ff834c648730e44342
Rohit Laheri <rohitdammy@gmail.com>
2022-06-17 09:27:24 +0530

    Update GitCheckOut.groovy

--- src/io/vz/pipelinejob/Factories/CheckOutFactory/GitCheckOut.groovy
+++ src/io/vz/pipelinejob/Factories/CheckOutFactory/GitCheckOut.groovy
@@ -54,7 +54,7 @@ public class GitCheckOut implements CheckOutCode, Serializable  {
         this.steps.echo param.branch
         this.steps.echo param.repoURL
         checkoutGit(param)
-        def changeLogSets = steps.currentBuild.changeSets
+       /* def changeLogSets = steps.currentBuild.changeSets
 for (int i = 0; i < changeLogSets.size(); i++) {
     def entries = changeLogSets[i].items
     for (int j = 0; j < entries.length; j++) {
@@ -70,7 +70,48 @@ for (int i = 0; i < changeLogSets.size(); i++) {
              steps.echo "pomchanged: true"   
             }
             else
-                steps.echo "pomchnaged: false"
+                steps.echo "pomchnaged: false" */
+         /*  def lastSuccessfulHash = null
+          def lastSuccessfulBuild = currentBuild.rawBuild.getPreviousSuccessfulBuild()
+          if ( lastSuccessfulBuild ) {
+            def scmAction = build?.actions.find { action -> action instanceof jenkins.scm.api.SCMRevisionAction }
+            lastSuccessfulHash = scmAction?.revision?.hash
+            def currentCommit = commitHashForBuild( currentBuild.rawBuild )
+              if (lastSuccessfulHash) {
+                commits = sh(
+                  script: "git rev-list $currentCommit \"^$lastSuccessfulHash\"",
+                  returnStdout: true
+                ).split('\n')
+                steps.echo "Commits are: $commits" 
+        
+             }
+            } */
+                
+                     def local_branch = steps.sh (
+                script: "git rev-parse --abbrev-ref HEAD",
+                label: "Getting current branch name",
+                returnStdout: true
+            ).trim()
+            steps.echo "Local branch is ${local_branch}"
+
+            def base_branch = 'master' 
+            // This is very naive.
+            // In reality, you need a better way to find out what your base branch is.
+            // One way is to have a file with a name of a base branch.
+            // Another one is to invoke API, e.g. GitHub API, to find out base branch.
+            // Use whatever works for you.
+            steps.echo "Base branch is ${base_branch}"
+
+            steps.sh script: "git fetch origin --no-tags ${base_branch}", label: "Getting base branch"
+
+            def git_diff = steps.sh (
+                script: "git diff --name-only origin/${base_branch}..${local_branch}",
+                returnStdout: true
+            ).trim()
+            steps.echo "git_diff" git_diff
+        
+        
+        
         }
     }
 }
