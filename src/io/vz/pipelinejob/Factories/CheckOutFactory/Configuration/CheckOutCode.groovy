d794ed3bead21662408bbfaf6382cee143ee40c7
Rohit Laheri <rohitdammy@gmail.com>
2022-06-21 09:35:31 +0530

    Update CheckOutCode.groovy

--- src/io/vz/pipelinejob/Factories/CheckOutFactory/Configuration/CheckOutCode.groovy
+++ src/io/vz/pipelinejob/Factories/CheckOutFactory/Configuration/CheckOutCode.groovy
@@ -2,5 +2,5 @@ package io.vz.pipelinejob.Factories.CheckOutFactory.Configuration
 
 //interface ScanCode
 public interface CheckOutCode extends Serializable {
-    void checkOut(Map param);
+    void checkOut(Map param, String parent);
 }
